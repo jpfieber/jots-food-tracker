89a3073e761d6484e175d0d05b8d03b4dba25873
Joseph Fieber <jpfieber@gmail.com>
2025-05-20 17:03:53 -0500

    changed default settings

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -28,14 +28,14 @@ export const DEFAULT_SETTINGS: FoodTrackerSettings = {
     foodFolder: "FoodTracker/Food",
     usdaFolder: "FoodTracker/USDA",
     recipesFolder: "FoodTracker/Recipes",
-    journalFolder: "Chronological/Journals",
-    journalNameFormat: "YYYY/YYYY-MM/YYYY-MM-DD", meals: [
+    journalFolder: "Chronological/Journals", journalNameFormat: "YYYY/YYYY-MM/YYYY-MM-DD", meals: [
         { name: "Breakfast", defaultTime: "07:00", emoji: "🕖" },
-        { name: "Morning Snack", defaultTime: "09:30", emoji: "🕙" },
-        { name: "Lunch", defaultTime: "12:00", emoji: "🕐" },
-        { name: "Afternoon Snack", defaultTime: "14:30", emoji: "🕓" },
-        { name: "Dinner", defaultTime: "17:30", emoji: "🕔" },
-        { name: "Evening Snack", defaultTime: "20:00", emoji: "🕗" }
+        { name: "Morning Snack", defaultTime: "09:30", emoji: "🕤" },
+        { name: "Lunch", defaultTime: "12:00", emoji: "🕛" },
+        { name: "Afternoon Snack", defaultTime: "14:30", emoji: "🕝" },
+        { name: "Dinner", defaultTime: "17:30", emoji: "🕠" },
+        { name: "Evening Snack", defaultTime: "20:00", emoji: "🕗" },
+        { name: "Recipe", defaultTime: "", emoji: "" }
     ],
     displayFooter: true,
     footerOrder: 1000,
